fc9e60f26fab7aaf100e094a7489b9ed91b115df
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-17 23:34:49 -0600

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,55 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let seconds= 0
+ let seconds2=0 
+ let minutes=0
+ let minutes2=0
+ let hours=0
+ let hours2 = 0;
+setInterval(()=> {
+    seconds++
+   if (seconds >= 10){
+   seconds = 0 
+   seconds2++
+   }
+   if (seconds2 >= 6){
+    seconds2 = 0
+    minutes++
+   }
+   if(minutes >= 10){
+    minutes= 0
+    minutes2++
+   }
+   if (minutes2 >= 6){
+   minutes2= 0
+   hours++
+   }
+  if(hours >= 10){
+    hours = 0
+    hours2++
+  }
+if(hours2 >= 9){
+    seconds = 0
+    seconds2 = 0
+    minutes = 0
+    minutes = 0
+    hours = 0
+    hours2 = 0
+}
+
+    
+    
+    ReactDOM.render(<Home 
+        
+        seconds={seconds}
+        seconds2={seconds2} 
+        minutes={minutes} 
+        minutes2={minutes2}
+        hours={hours}
+        hours2={hours2}
+         />, document.querySelector("#app"));
+
+}, 1000);
+
